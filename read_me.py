2abd5350dc9b12bfcc04eceddd85ada2b4d6c932
alexxnicolas <alexandrenicolas3@gmail.com>
2021-02-09 19:28:23 +0100

    well played

--- read_me.py
+++ read_me.py
@@ -1,3 +1,18 @@
-est-ce que ça marche
+a=1
+print(a)
+type(a)
+b=0.1
+a=b
+True+False
+z=2+5j
+type(z)
+import math
+math.cos(math.pi)
+dir(math)
 
-fhjhdfjh
+def power(n):
+    i = 0
+    while (2**i < n):
+        i += 1
+    return i
+power(12345)
